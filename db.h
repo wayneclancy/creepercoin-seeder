2bfb2cef2ba59aaa06a9e516afff3db5c6919413
root <root@magicbun2.vm.bytemark.co.uk>
2014-01-28 12:10:48 +0000

    Tweaked block size and connection string for creepercoin -WC

--- db.h
+++ db.h
@@ -16,7 +16,7 @@
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 0 : 470000;
+    return testnet ? 0 : 7;
 }
 
 std::string static inline ToString(const CService &ip) {
--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xfb, 0xc0, 0xb6, 0xdb };
+unsigned char pchMessageStart[4] = { 0x1b, 0xc2, 0xb2, 0xda };
 
 CMessageHeader::CMessageHeader()
 {
